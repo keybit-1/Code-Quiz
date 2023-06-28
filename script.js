12f7559bbabeba09beebe05175fa74cb47393c45
keybit-1 <131838974+keybit-1@users.noreply.github.com>
2023-06-28 18:05:15 -0500

    Added more functions to flesh out design

--- script.js
+++ script.js
@@ -61,9 +61,29 @@ var quizData = [
   var choicesEl = document.getElementById("choices");
   var resultScreenEl = document.getElementById("result-screen");
   var scoreEl = document.getElementById("score");
-  var timerEl = document.getElementById("timer"); // Added timer element
+  var timerEl = document.getElementById("timer");
   var initialsForm = document.getElementById("initials-form");
   var initialsInput = document.getElementById("initials");
+  
+  // Load leaderboard data and display it on the landing page
+  function loadLeaderboard() {
+    var leaderboardContainer = document.getElementById("leaderboard-container");
+    var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+  
+    if (!leaderboardContainer) {
+      console.error("Leaderboard container not found.");
+      return;
+    }
+  
+    leaderboardContainer.innerHTML = "";
+  
+    highscores.forEach(function (scoreData) {
+      createLeaderboardCard(scoreData.initials, scoreData.score);
+    });
+  }
+  
+  // Call the loadLeaderboard function when the page is loaded
+  window.addEventListener("load", loadLeaderboard);
   
   // Start the quiz
   function startQuiz() {
@@ -100,33 +120,30 @@ var quizData = [
     submitButton.addEventListener("click", submitAnswer);
     choicesEl.appendChild(submitButton);
   
-    clearInterval(timerId); // Clear the current timer
-    timerId = setInterval(updateTimer, 1000); // Start the timer again
+    clearInterval(timerId);
+    timerId = setInterval(updateTimer, 1000);
   }
   
   // Submit the selected answer
   function submitAnswer() {
     var selectedChoice = choicesEl.querySelector("input[type='radio']:checked");
     if (!selectedChoice) {
-      return; // No answer selected
+      return;
     }
   
     var selectedAnswer = selectedChoice.value;
     var question = quizData[currentQuestionIndex];
   
     if (selectedAnswer === question.answer) {
-      // Correct answer
-      score += 10; // Each correct answer is worth 10 points
+      score += 10;
       selectedChoice.parentNode.classList.add("correct");
       currentQuestionIndex++;
     } else {
-      // Incorrect answer
-      timeLeft -= 60; // Subtract 60 seconds (1 minute) for an incorrect answer
+      timeLeft -= 60;
       if (timeLeft < 0) {
-        timeLeft = 0; // Ensure that timeLeft is not negative
+        timeLeft = 0;
       }
       selectedChoice.parentNode.classList.add("incorrect");
-      // Display the notification message
       var notificationEl = document.getElementById("notification");
       notificationEl.textContent = "Incorrect! 1 minute has been deducted.";
       notificationEl.classList.add("incorrect");
@@ -146,11 +163,32 @@ var quizData = [
   // Save the score and restart the quiz
   function saveScoreAndRestart(event) {
     event.preventDefault();
+    var initials = initialsInput.value.toUpperCase();
   
-    // Save the score (you can implement the logic here to save the score)
+    if (initials) {
+      var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+      var existingScore = highscores.find(function (scoreData) {
+        return scoreData.initials === initials;
+      });
   
-    // Restart the quiz
-    location.reload();
+      if (existingScore) {
+        alert("You have already saved a score. Only one score per user is allowed.");
+        return;
+      }
+  
+      var scoreData = {
+        initials: initials,
+        score: score,
+      };
+  
+      highscores.push(scoreData);
+      highscores.sort((a, b) => b.score - a.score);
+      highscores = highscores.slice(0, 10);
+      localStorage.setItem("highscores", JSON.stringify(highscores));
+      createLeaderboardCard(initials, score);
+    }
+  
+    initialsForm.reset();
   }
   
   // End the quiz
@@ -158,7 +196,7 @@ var quizData = [
     clearInterval(timerId);
     document.getElementById("quiz-screen").classList.add("hidden");
     resultScreenEl.classList.remove("hidden");
-    scoreEl.textContent = Math.min(Math.floor((score / (quizData.length * 10)) * 100), 100); // Calculate the score percentage
+    scoreEl.textContent = Math.min(Math.floor((score / (quizData.length * 10)) * 100), 100);
   
     var refreshButton = document.createElement("button");
     refreshButton.id = "refresh-button";
@@ -167,12 +205,53 @@ var quizData = [
     resultScreenEl.appendChild(refreshButton);
   }
   
+  // Create leaderboard card
+  function createLeaderboardCard(initials, score) {
+    var leaderboardContainer = document.getElementById("leaderboard-container");
+  
+    if (!leaderboardContainer) {
+      console.error("Leaderboard container not found.");
+      return;
+    }
+  
+    var card = document.createElement("div");
+    card.classList.add("leaderboard-card");
+  
+    var initialsEl = document.createElement("span");
+    initialsEl.classList.add("initials");
+    initialsEl.textContent = initials;
+  
+    var scoreContainer = document.createElement("div");
+    scoreContainer.classList.add("score-container");
+  
+    var scoreLabel = document.createElement("span");
+    scoreLabel.classList.add("score-label");
+    scoreLabel.textContent = "Score:";
+  
+    var scoreEl = document.createElement("span");
+    scoreEl.classList.add("score");
+    scoreEl.textContent = score;
+  
+    var deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "x";
+    deleteButton.addEventListener("click", deleteScore);
+  
+    scoreContainer.appendChild(scoreLabel);
+    scoreContainer.appendChild(scoreEl);
+  
+    card.appendChild(initialsEl);
+    card.appendChild(scoreContainer);
+    card.appendChild(deleteButton);
+  
+    leaderboardContainer.appendChild(card);
+  }
+  
   // Update the timer
   function updateTimer() {
-    var minutes = Math.floor(timeLeft / 60); // Calculate the minutes
-    var seconds = timeLeft % 60; // Calculate the remaining seconds
+    var minutes = Math.floor(timeLeft / 60);
+    var seconds = timeLeft % 60;
   
-    // Format the time with leading zeros if needed
     var formattedTime = minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
   
     timerEl.textContent = "Time: " + formattedTime;
@@ -189,9 +268,33 @@ var quizData = [
     location.reload();
   }
   
+  // Delete a score from the leaderboard
+  function deleteScore(event) {
+    var card = event.target.closest(".leaderboard-card");
+    var initials = card.querySelector(".initials").textContent;
+    var score = card.querySelector(".score").textContent;
+  
+    var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+    highscores = highscores.filter(function (scoreData) {
+      return scoreData.initials !== initials || scoreData.score !== parseInt(score);
+    });
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+  
+    card.remove();
+  
+    loadLeaderboard();
+  }
+  
   // Assign event listeners
   startButton.addEventListener("click", startQuiz);
   initialsForm.addEventListener("submit", saveScoreAndRestart);
+  
+
+  
+  
+
+
+  
   
   
   
